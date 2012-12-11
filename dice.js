9957e33fc40fe89c264b6c08e06c93cc511dae66
Robin Garen Aaberg <technocake@158-37-84-77.hib.no>
2012-12-11 21:23:08 +0100

    3d dice is rolling

--- dice.js
+++ dice.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   
    $("#rollbtn").click(function(event){
    	n = Math.floor((Math.random()*6)+1);
-     $("p").attr('class', 'd' + n);
-     console.log(n);
+     $("#cube").attr('class', 'show-d' + n);
+     console.log('dice is: ' + n);
    });
  });
 
