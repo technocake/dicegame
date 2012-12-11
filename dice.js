5506d535fbc0aa35b5d1c2028553f063a9721220
Robin Garen Aaberg <technocake@158-37-84-77.hib.no>
2012-12-11 23:00:20 +0100

    hilight and roll on click feature added :)

--- dice.js
+++ dice.js
@@ -2,11 +2,15 @@
 	
  */
 $(document).ready(function(){
+
+
+	function roll(event) {
+		n = Math.floor((Math.random()*6)+1);
+	    $("#cube").attr('class', 'show-d' + n);
+	    console.log('dice is: ' + n);
+	}
   
-   $("#rollbtn").click(function(event){
-   	n = Math.floor((Math.random()*6)+1);
-     $("#cube").attr('class', 'show-d' + n);
-     console.log('dice is: ' + n);
-   });
+   $("#rollbtn").click(roll);
+   $("#cube figure p").click(roll);
  });
 
